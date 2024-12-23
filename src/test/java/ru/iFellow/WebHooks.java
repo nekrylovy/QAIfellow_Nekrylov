5c7f952155e53b9e45bcedf8281840ccb2576ad7
Yriy Nekrylov <nekrylov.y@gmail.com>
2024-12-23 21:08:28 +0400

    Настройки логгера вынесены под аннотацию BeforeAll

--- src/test/java/ru/iFellow/WebHooks.java
+++ src/test/java/ru/iFellow/WebHooks.java
@@ -5,6 +5,7 @@ import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.PageLoadStrategy;
 import ru.iFellow.pages.JiraAuthorizationPage;
@@ -16,13 +17,17 @@ public class WebHooks {
     static final JiraAuthorizationPage authPage = new JiraAuthorizationPage();
     final Config conf = new Config();
 
-    @BeforeEach
-    public void initBrowser() {
-        Configuration.pageLoadStrategy = PageLoadStrategy.EAGER.toString();
+    @BeforeAll
+    public static void initLogger() {
         SelenideLogger.addListener("AllureSelenide",
                 new AllureSelenide()
                         .screenshots(true)
                         .savePageSource(true));
+    }
+
+    @BeforeEach
+    public void initBrowser() {
+        Configuration.pageLoadStrategy = PageLoadStrategy.EAGER.toString();
         Selenide.open(conf.getUrl());
         getWebDriver().manage().window().maximize();
     }
